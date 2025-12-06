650f9e6027f70f269286b34a72902616d452b641
harshit2503 <113522267+harshit2503@users.noreply.github.com>
2025-12-06 19:29:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-portfolio/",   // 👈 REQUIRED FOR GITHUB PAGES
   css: {
     modules: {
       localsConvention: "camelCase",
